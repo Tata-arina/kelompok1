e4c95f42ca8aba6e1e028c6c4ab51c9026e4b6c1
unknown <nastagsi30a@gmail.com>
2023-09-01 11:12:29 +0700

    membuat halaman model

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -2,16 +2,19 @@
 @section('title')Beranda @endsection
 @section('content')
 <section id="Beranda">
-    <div class="container-fluid" id="baground" >
-    <div class="container text-center" style="margin-top: 70px;" id="pembuka" >  
-     <h1 class="text-secondary-emphasis fw-bold">PENERIMAAN PESERTA DIDIK BARU <br> SMA KUSUMA BANGSA 2023/2024 <br> Registrasi Online melalui</h1> 
-     <a href="portal" ><button type="button" class="btn btn-primary m-3">Portal SMA KUMBANG</button></a>
+  <div class="container text-center mt-5">
+    <div class="card-body">
+    <h1 class="h2">Selamat Datang di Portal Layanan SIAP PPDB Online</h2>
+    <p class="lh-sm">Layanan SIAP PPDB Online adalah sebuah sistem layanan yang dirancang untuk melakukan otomasi seleksi Pendaftaran Peserta Didik Baru (PPDB), 
+    mulai dari proses pendaftaran, proses seleksi hingga pengumuman hasil seleksi, yang dilakukan secara online dan berbasis waktu nyata (realtime).</p>
   </div>
   </div>
   </section>
   <!-- end jumbotron -->
 
+
 <!-- main -->
 <main>
-<!-- tentang -->
+<!-- beranda -->
+
 @endsection 
